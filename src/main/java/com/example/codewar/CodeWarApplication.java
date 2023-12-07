a3b234b957db1270a2fb8388caf6eb39c8e39da6
LordNedStark <osisiogubenjamin1@gmail.com>
2023-12-07 16:22:08 +0100

    first commit

---
+++ src/main/java/com/example/codewar/CodeWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.codewar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeWarApplication.class, args);
+	}
+
+}
